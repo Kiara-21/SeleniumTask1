e07636ed4b7d2189ee7366f73ffad3a5b5629d0b
Kiara Bodiu <bodyukiara@gmail.com>
2024-04-27 16:01:21 +0300

    solution

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace SeleniumPractice
+{
+    public class SeleniumTask
+    {
+        public static void Main()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://pastebin.com/");
+
+            IWebElement textBox = driver.FindElement(By.XPath("//*[@id=\"postform-text\"]"));
+            textBox.SendKeys("Hello from WebDriver");
+
+            IWebElement textTitle = driver.FindElement(By.XPath("//*[@id=\"postform-name\"]"));
+            textTitle.SendKeys("helloweb");
+
+            IWebElement sendButton = driver.FindElement(By.XPath("//*[@id=\"w0\"]/div[5]/div[1]/div[10]/button"));
+            sendButton.Click();
+
+            Thread.Sleep(5000);
+            driver.Quit();
+        }
+    }
+}
